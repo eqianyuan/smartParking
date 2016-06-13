abbfcf8f87c1a4d0c6e3b8ce6475a1c994cb309f
jason <422065325@qq.com>
2016-06-13 20:35:26 +0800

    测试集成推送

---
+++ src/main/java/cn/eqianyuan/smartParking/common/exception/EqianyuanException.java
@@ -0,0 +1,12 @@
+package cn.eqianyuan.smartParking.common.exception;
+
+/**
+ * 自定义异常类
+ * Created by jason on 2016-05-19.
+ */
+public class EqianyuanException extends Exception {
+
+    public EqianyuanException(String msg){
+        super(msg);
+    }
+}
